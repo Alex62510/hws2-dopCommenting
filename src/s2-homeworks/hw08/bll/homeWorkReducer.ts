a561ac099ace0be144fbdab61902b75f13adedd1
Alex62510 <eagle625@tut.by>
2023-04-24 16:01:13 +0300

    commit-08 is done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,23 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
+        case 'sort': {
+            const newState = [...state]
+            if (action.payload === "up") {
+                newState.sort((a, b) => a.name > b.name ? 1 : -1)
+            }
+            if (action.payload === "down") {
+                newState.sort((a, b) => a.name < b.name ? 1 : -1)
+            }
+            // by name
             // sort() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            return newState // need to fix
         }
         case 'check': {
             // filter() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            return state.filter(el => el.age > action.payload) // need to fix
         }
         default:
             return state
